55a450a7bc89fc8effc9f43a87bc96f8e706a91d
Shuaib Hussein <93594848+Shuaybh97@users.noreply.github.com>
2025-11-24 13:28:03 +0000

    fixing output

--- terraform/bootstrap/outputs.tf
+++ terraform/bootstrap/outputs.tf
@@ -4,5 +4,5 @@ output "github-oidc-provider-arn" {
 }
 output "state_bucket_name" {
   description = "Name of the S3 bucket for Terraform state"
-  value       = aws_s3_bucket.terraform_state.bucket
+  value       = aws_s3_bucket.tf_state.bucket
 }
